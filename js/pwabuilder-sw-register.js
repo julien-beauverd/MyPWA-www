071658ed453edb3cb3c278288993bc0c4e917956
JulienBeauverd <julien.beauverd@heig-vd.ch>
2019-03-10 23:54:39 +0100

    add service worker

--- js/pwabuilder-sw-register.js
+++ js/pwabuilder-sw-register.js
@@ -3,13 +3,14 @@
 // Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
 
 // Check compatibility for the browser we're running this in
+
 if ("serviceWorker" in navigator) {
   if (navigator.serviceWorker.controller) {
     console.log("[PWA Builder] active service worker found, no need to register");
   } else {
     // Register the service worker
     navigator.serviceWorker
-      .register("pwabuilder-sw.js", {
+      .register("/js/pwabuilder-sw.js", {
         scope: "./"
       })
       .then(function (reg) {
